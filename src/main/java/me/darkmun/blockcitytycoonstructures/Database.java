f51a887a9ed8cc5b493a2b457746246628543dfe
Darkmun73 <mrgame7337@gmail.com>
2024-08-07 15:55:20 +1000

    Исправлен баг

--- src/main/java/me/darkmun/blockcitytycoonstructures/Database.java
+++ src/main/java/me/darkmun/blockcitytycoonstructures/Database.java
@@ -1,15 +1,17 @@
 package me.darkmun.blockcitytycoonstructures;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import org.bukkit.Bukkit;
+
+import java.sql.*;
+import java.util.logging.Level;
 
 public class Database {
     private Connection connection = null;
+    private int keepAlive = 0;
 
     public Connection getConnection() throws SQLException {
-        if (connection == null) {
+        if (connection == null || connection.isClosed()) {
+            Bukkit.getScheduler().cancelTask(keepAlive);
             java.util.Properties conProperties = new java.util.Properties();
             conProperties.put("user", "u95570_LRzuS0M9U7");
             conProperties.put("password", "uMGeUJbmt!oH^FYk^I1VSSTW");
@@ -17,6 +19,14 @@ public class Database {
             conProperties.put("maxReconnects", "15");
             String url = "jdbc:mysql://mysql2.joinserver.xyz:3306/s95570_BlockCityTycoon";
             connection = DriverManager.getConnection(url, conProperties);
+            keepAlive = Bukkit.getScheduler().runTaskTimerAsynchronously(BlockCityTycoonStructures.getPlugin(), () -> {
+                try (PreparedStatement ps = connection.prepareStatement("SELECT 1 FROM chunk_data")) {
+                    ps.executeQuery();
+                } catch (SQLException ex) {
+                    Bukkit.getLogger().log(Level.SEVERE, "Keep alive failed", ex);
+                    ex.printStackTrace();
+                }
+            }, 0, 72000).getTaskId();
         }
         return connection;
     }
